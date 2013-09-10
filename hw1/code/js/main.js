cd002fc53cf80c1766d14eba6ff3d820c3981e0b
Owen Herterich <oherterich@gmail.com>
2013-09-10 01:45:23 -0400

    added keyboard functionality, removed console.logs, added comments

--- hw1/code/js/main.js
+++ hw1/code/js/main.js
@@ -1,11 +1,23 @@
+//Variables to keep track of the active words on the page, acccording to the index of the array.
 var adjectiveIndex = 0;
 var nounIndex = 0;
 var verbIndex = 0;
 
+//Array of words that will be displayed on the page.
 var adjective = [ "stubborn" , "innovative", "bacon-appreciating", "punctual", "happy", "nostalgic", "book-hoarding", "contemplative", "drama-loving" ];
 var noun = [ "folk music connoiseur", "Mad Men fan", "information designer", "GIF-addict", "Texan", "dog-lover", "creative coder", "motion graphics guy" ];
 var verb = [ "make things pretty." , "solve problems.", "drink craft beer." , "program every once in a while.", "buy nice shoes.", "visualize interesting data.", "explore the ironic.", "quote Bob Dylan." ];
 
+
+//Functionality for generating random words. Works with spacebar, left arrow, and right arrow.
+document.onkeydown=function(e) {
+	if (e.which == 32 || e.which == 37 || e.which == 39) {
+		setRandom();
+	}
+}
+
+
+//This function generates a random number for use in the index of each word array and changes the HTML accordingly.
 function setRandom() {
 
 	adjectiveIndex = Math.floor(Math.random() * (adjective.length));
@@ -17,6 +29,7 @@ function setRandom() {
 	document.getElementById("verbWord").innerHTML = verb[verbIndex];
 }
 
+//This function changes the adjective on the page if the user clicked the arrow buttons or the word itself.
 function changeAdjective(div) {
 	var id = div.id;
 
@@ -39,7 +52,6 @@ function changeAdjective(div) {
 	}
 
 	if (id == "adjectiveWord") {
-		console.log(adjectiveIndex);
 		if (adjectiveIndex == adjective.length-1) {
 			adjectiveIndex = 0;
 		}
@@ -51,6 +63,7 @@ function changeAdjective(div) {
 	document.getElementById("adjectiveWord").innerHTML = adjective[adjectiveIndex];
 }
 
+//This function changes the noun on the page if the user clicked the arrow buttons or the word itself.
 function changeNoun(div) {
 	var id = div.id;
 
@@ -84,6 +97,7 @@ function changeNoun(div) {
 	document.getElementById("nounWord").innerHTML = noun[nounIndex];
 }
 
+//This function changes the verb on the page if the user clicked the arrow buttons or the word itself.
 function changeVerb(div) {
 	var id = div.id;
 
