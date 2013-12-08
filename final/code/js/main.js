e55853e56c493d60cb4d95c65d163c48788b0cf9
Owen Herterich <oherterich@gmail.com>
2013-12-07 23:39:54 -0500

    added lots of new textures, as well as objs for the link names

--- final/code/js/main.js
+++ final/code/js/main.js
@@ -8,10 +8,17 @@ var starMaterial, starGeometry;
 var starColors = new Array();
 var starOffsets = new Array();
 
-var planets = new Array();
+var planets = new Array(); //Actual planet meshes
+var planetNames = new Array(); //Array of titles for the different planets
+
+var logoTitle, logoDisc; //Meshes for the logo
 
 var centerOrbit = new Array(); //Main orbits for the planets
 var moonOrbit;
+
+var cloudMesh;
+
+var planetTheta = 0.001;
 
 //To move light
 var theta = 0;
@@ -57,7 +64,7 @@ function init() {
 	//Add a point in which to orbit around
 	for (var i = 0; i < 11; i++) {
 		orbit = new THREE.Object3D();
-		orbit.rotation.set(Math.PI * Math.random(), Math.PI * Math.random(), 0);
+		//orbit.rotation.set(0, 0, Math.PI * 2 * Math.random());
 		centerOrbit.push( orbit );
 		mainCube.add( centerOrbit[i] );
 	}
@@ -72,6 +79,81 @@ function init() {
 
 	hemiLight = new THREE.HemisphereLight(0xFFFFFF);
 	//scene.add(hemiLight);
+
+	/*********LOAD OBJ*********************/
+	var loader = new THREE.JSONLoader();
+ 
+	var createTextMesh = function( geometry, i, material )
+	{
+		var zMat = material;
+	    var zmesh = new THREE.Mesh( geometry, zMat );
+	    zmesh.position.set( 0, 0, 50 );
+	    zmesh.scale.set( 15, 15, 15 );
+	    zmesh.overdraw = true;
+	    planetNames.push( zmesh );
+	    planets[i].add( zmesh );
+	};
+
+	var textMat = new THREE.MeshLambertMaterial({ color: 0xD9CC14 })
+ 
+ 	//Load all of the site names
+	callbackKey = function(geometry) {createTextMesh(geometry, 0, textMat)};
+	loader.load( "js/obj/jamcentral.js", callbackKey );
+
+	callbackKey = function(geometry) {createTextMesh(geometry, 1, textMat)};
+	loader.load( "js/obj/planetbball.js", callbackKey );
+
+	callbackKey = function(geometry) {createTextMesh(geometry, 2, textMat)};
+	loader.load( "js/obj/lunartunes.js", callbackKey );
+
+	callbackKey = function(geometry) {createTextMesh(geometry, 3, textMat)};
+	loader.load( "js/obj/jumpstation.js", callbackKey );
+
+	callbackKey = function(geometry) {createTextMesh(geometry, 4, textMat)};
+	loader.load( "js/obj/warnerstudiostore.js", callbackKey );
+
+	callbackKey = function(geometry) {createTextMesh(geometry, 5, textMat)};
+	loader.load( "js/obj/behindthejam.js", callbackKey );
+
+	callbackKey = function(geometry) {createTextMesh(geometry, 6, textMat)};
+	loader.load( "js/obj/sitemap.js", callbackKey );
+
+	callbackKey = function(geometry) {createTextMesh(geometry, 7, textMat)};
+	loader.load( "js/obj/stellarsouvenirs.js", callbackKey );
+
+	callbackKey = function(geometry) {createTextMesh(geometry, 8, textMat)};
+	loader.load( "js/obj/juniorjam.js", callbackKey );
+
+	callbackKey = function(geometry) {createTextMesh(geometry, 9, textMat)};
+	loader.load( "js/obj/thelineup.js", callbackKey );
+
+	callbackKey = function(geometry) {createTextMesh(geometry, 10, textMat)};
+	loader.load( "js/obj/pressboxshuttle.js", callbackKey );
+
+	var createLogoMesh = function( geometry, material )
+	{
+		var zMat = material;
+	    var zmesh = new THREE.Mesh( geometry, zMat );
+	    zmesh.position.set( 0, 0, 0 );
+	    zmesh.scale.set( 4.2, 4.2, 4.2 );
+	    zmesh.overdraw = true;
+	    mainCube.add( zmesh );
+	};
+
+	var discText = THREE.ImageUtils.loadTexture("img/textures/spacejam/spacejam_disc.png");
+	var discMat = new THREE.MeshPhongMaterial({ map: discText });
+
+	var logoFrontMat = new THREE.MeshPhongMaterial({ color: 0x12544C, ambient: 0x12544C, specular:0x555555, shininess: 100, reflectivity: 100 });
+	var logoOutlineMat = new THREE.MeshPhongMaterial( { color: 0x999999, ambient: 0x999999, specular:0x555555, shininess: 100, reflectivity: 100 } )
+
+	callbackKey = function(geometry) {createLogoMesh(geometry, logoFrontMat)};
+	loader.load( "js/obj/spacejam_front.js", callbackKey );
+
+	callbackKey = function(geometry) {createLogoMesh(geometry, logoOutlineMat)};
+	loader.load( "js/obj/spacejam_outlines.js", callbackKey );
+
+	callbackKey = function(geometry) {createLogoMesh(geometry, discMat)};
+	loader.load( "js/obj/spacejam_disc.js", callbackKey );
 
 	/*******ADD PARTICLES********/
 	starGeometry = new THREE.Geometry();
@@ -98,8 +180,6 @@ function init() {
 
 	starGeometry.colors = starColors;
 
-	console.log(starGeometry);
-
 	starMaterial = new THREE.ParticleSystemMaterial( { size: 3, sizeAttenuation: false, map: sprite, vertexColors: true, transparent: true } );
 	starMaterial.color.setHSL( 1.0, 0.0, 0.9 );
 
@@ -109,10 +189,11 @@ function init() {
 
 
 	for (var i = 0; i < 11; i++) {
-		var planetGeometry = new THREE.SphereGeometry(Math.random() * 25 + 25, 64, 64);
+		var planetGeometry = new THREE.SphereGeometry(40, 32, 32);
 
 		var c = new THREE.Color( 0xFFFFFF );
-		c.setRGB( Math.random(), Math.random(), Math.random() );
+		//c.setRGB( Math.random(), Math.random(), Math.random() );
+		c.setRGB(1.0,1.0,1.0);
 
 		var planetTexture = THREE.ImageUtils.loadTexture("img/textures/moon.png");
 		planetTexture.repeat.set( 4, 2 );
@@ -124,25 +205,254 @@ function init() {
 		planetBump.wrapS = planetBump.wrapT = THREE.RepeatWrapping;
 		planetBump.anisotropy = 16;
 
-		var planetMaterial = new THREE.MeshPhongMaterial( { map: planetTexture, bump: planetBump, bumpScale: 1000, color: c });
+		var planetMaterial = new THREE.MeshPhongMaterial( { map: planetTexture, bump: planetBump, bumpScale: 0.4, color: c });
 		var planet = new THREE.Mesh( planetGeometry, planetMaterial );
 
-		planet.position.set( getRandomInt(250, 500), getRandomInt(250, 500), 0);
+		planet.position.set( 0, 300, 0);
 		planets.push( planet );
 
 		centerOrbit[i].add( planets[i] );
 	}
+	/***************************************************************************/
+	/*****************************Planet 0 - Jam Central******************************/
+	/***************************************************************************/
+	centerOrbit[0].rotation.set(0, 0, degToRad(-5));
+
+	planets[0].position.set(0, 280, 0);
+
+	planets[0].material.map = new THREE.ImageUtils.loadTexture("img/textures/earth/earth_color.png");
+	planets[0].material.bumpMap = new THREE.ImageUtils.loadTexture("img/textures/earth/earth_bump.png");
+	planets[0].material.bumpScale = 0.05;
+	planets[0].material.specularMap = new THREE.ImageUtils.loadTexture("img/textures/earth/earth_spec.png");
+	planets[0].material.specular = new THREE.Color( 0x333333 );
+
+	//Add clouds to Earth;
+	var cloudTexture = new THREE.ImageUtils.loadTexture("img/textures/earth/earth_cloud_trans.png");
+	var cloudGeometry   = new THREE.SphereGeometry(40.1, 32, 32)
+	var cloudMaterial  = new THREE.MeshPhongMaterial({
+	  map     : cloudTexture,
+	  side        : THREE.DoubleSide,
+	  opacity     : 0.3,
+	  transparent : true,
+	  depthWrite  : false,
+	})
+	cloudMesh = new THREE.Mesh(cloudGeometry, cloudMaterial)
+	planets[0].add(cloudMesh)
 
 	moonOrbit = new THREE.Object3D();
 	planets[0].add( moonOrbit );
 
-	var moonGeometry = new THREE.SphereGeometry( 10, 32, 32);
-	var moonMaterial = new THREE.MeshPhongMaterial( { color: 0x660000 } );
+	//Add moon to Earth
+	var moonGeometry = new THREE.SphereGeometry( 7, 32, 32);
+	var moonTexture = new THREE.ImageUtils.loadTexture("img/textures/earth/moon.png");
+	var moonBump = new THREE.ImageUtils.loadTexture("img/textures/earth/moon_bump.png");
+	var moonMaterial = new THREE.MeshPhongMaterial( { map: moonTexture, bumpMap: moonBump, bumpScale: 0.05 } );
 	var moon = new THREE.Mesh( moonGeometry, moonMaterial );
-	moon.position.set( 50, 50, 0);
+	moon.position.set( 40, 40, 0);
 
 	moonOrbit.add( moon );
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
 
+
+
+	/***************************************************************************/
+	/*****************************Planet 1 - Planet B-Ball******************************/
+	/***************************************************************************/
+	planets[1].scale.set(0.9, 0.9, 0.9);
+	planets[1].position.set(0,320,0);
+
+	centerOrbit[1].rotation.set( 0, 0, degToRad(22) );
+
+	planets[1].material.map = new THREE.ImageUtils.loadTexture("img/textures/basketball/basketball.png");
+	planets[1].material.bumpMap = new THREE.ImageUtils.loadTexture("img/textures/basketball/basketball_bump.png");
+	planets[1].material.bumpScale = 0.05;
+	planets[1].material.specularMap = new THREE.ImageUtils.loadTexture("img/textures/basketball/basketball_spec.png");
+	planets[1].material.specular = new THREE.Color( 0x111111 );
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
+
+
+
+	/***************************************************************************/
+	/**************************Planet 2 - Lunar Tunes***************************/
+	/***************************************************************************/
+	centerOrbit[2].rotation.set( 0, 0, degToRad(52) );
+
+	planets[2].scale.set(1.2, 1.2, 1.2);
+	planets[2].position.set(0, 350, 0);
+
+	planets[2].material.map = new THREE.ImageUtils.loadTexture("img/textures/rock/rock.png");
+	planets[2].material.bumpMap = new THREE.ImageUtils.loadTexture("img/textures/rock/rock_bump.png");
+	planets[2].material.bumpScale = 0.1;
+	planets[2].material.specular = new THREE.Color( 0x444444 );
+	var c = new THREE.Color( 0x0089EB ); //lightish blue
+	planets[2].material.color = c;
+	// planets[2].material.transparent = true;
+	// planets[2].material.opacity = 0.0;
+
+	// var ringGeometry = new THREE.TorusGeometry( 70, 5, 50, 50);
+	// var ringMaterial = new THREE.MeshPhongMaterial( { color: 0xFF0000 } );
+	// var ringMesh = new THREE.Mesh( ringGeometry, ringMaterial );
+	// planets[2].add( ringMesh );
+
+	var saturnRing = new THREEx.Planets._RingGeometry(60, 80, 512);
+	var ringMaterial = new THREE.MeshPhongMaterial( { 
+		color: 0xFF0000,
+		side : THREE.DoubleSide,
+		transparent: true,
+		opacity: 0.5 
+	});
+	var ringMesh = new THREE.Mesh( saturnRing, ringMaterial );
+	ringMesh.rotation.set(Math.PI / 6, -Math.PI / 9, 0);
+	planets[2].add( ringMesh );
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
+
+
+
+	/***************************************************************************/
+	/**************************Planet 3 - Jump Station**************************/
+	/***************************************************************************/
+	planets[3].position.set(0, 350, 0);
+	planets[3].scale.set(0.65, 0.65, 0.65);
+
+	centerOrbit[3].rotation.set( 0, 0, degToRad(80) );
+
+	planets[3].material.map = new THREE.ImageUtils.loadTexture("img/textures/cloud/cloud.png");
+	planets[3].material.specular = new THREE.Color( 0x000000 );
+	var c = new THREE.Color( 0x85FA25 ); //lightish blue
+	planets[3].material.color = c;
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
+
+
+
+	/***************************************************************************/
+	/**********************Planet 4 - Warner Studio Store************************/
+	/***************************************************************************/
+	centerOrbit[4].rotation.set( 0, 0, degToRad(108) );
+
+	planets[4].position.set( 0, 320, 0 );
+	planets[4].scale.set(1.05, 1.05, 1.05);
+
+	planets[4].material.map = new THREE.ImageUtils.loadTexture("img/textures/tigerstripe/tigerstripe_orange.png");
+	planets[4].material.bumpMap = new THREE.ImageUtils.loadTexture("img/textures/tigerstripe/tigerstripe_orange_bump.png");
+	planets[4].material.bumpScale = 0.05;
+	planets[4].material.specular = new THREE.Color( 0x333333 );
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
+
+
+
+	/***************************************************************************/
+	/************************Planet 5 - Behind the Jam**************************/
+	/***************************************************************************/
+	centerOrbit[5].rotation.set( 0, 0, degToRad(150) );
+
+	planets[5].position.set( 0, 290, 0 );
+	planets[5].scale.set( 0.9, 0.9, 0.9 );
+
+	planets[5].material.map = new THREE.ImageUtils.loadTexture("img/textures/tigerstripe/tigerstripe_blue.png");
+	planets[5].material.bumpMap = new THREE.ImageUtils.loadTexture("img/textures/tigerstripe/tigerstripe_blue_bump.png");
+	planets[5].material.bumpScale = 0.2;
+	planets[5].material.specular = new THREE.Color( 0x111111 );
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
+
+
+
+	/***************************************************************************/
+	/***************************Planet 6 - Site Map*****************************/
+	/***************************************************************************/
+
+	centerOrbit[6].rotation.set( 0, 0, degToRad(185) );
+
+	planets[6].position.set( 0, 290, 0 );
+	planets[6].scale.set( 0.9, 0.9, 0.9 );
+
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
+
+
+
+	/***************************************************************************/
+	/***********************Planet 7 - Stellar Souvenirs************************/
+	/***************************************************************************/
+	centerOrbit[7].rotation.set( 0, 0, degToRad(230) );
+
+	planets[7].position.set( 0, 290, 0 );
+	planets[7].scale.set( 1.3, 1.3, 1.3 );
+
+	planets[7].material.map = new THREE.ImageUtils.loadTexture("img/textures/cloud/cloud.png");
+	planets[7].material.specular = new THREE.Color( 0x000000 );
+
+	var c = new THREE.Color( 0x22C5F2 ); //light cyan
+	planets[7].material.color = c;
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
+
+
+
+	/***************************************************************************/
+	/**************************Planet 8 - Junior Jam****************************/
+	/***************************************************************************/
+	centerOrbit[8].rotation.set( 0, 0, degToRad(265) );
+
+	planets[8].position.set( 0, 320, 0 );
+	planets[8].scale.set( 0.8, 0.8, 0.8 );
+
+	planets[8].material.map = new THREE.ImageUtils.loadTexture("img/textures/tigerstripe/tigerstripe_green.png");
+	planets[8].material.bumpMap = new THREE.ImageUtils.loadTexture("img/textures/tigerstripe/tigerstripe_green_bump.png");
+	planets[8].material.bumpScale = 0.03;
+	planets[8].material.specular = new THREE.Color( 0x222222 );
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
+
+
+
+	/***************************************************************************/
+	/**************************Planet 9 - The Lineup****************************/
+	/***************************************************************************/
+	centerOrbit[9].rotation.set( 0, 0, degToRad(290) );
+
+	planets[9].position.set(0, 310, 0);
+	planets[9].scale.set(0.75, 0.75, 0.75);
+
+	planets[9].material.map = new THREE.ImageUtils.loadTexture("img/textures/moon/moon.png");
+	planets[9].material.bumpMap = new THREE.ImageUtils.loadTexture("img/textures/moon/moon_bump.png");
+	planets[9].material.bumpScale = 0.3;
+	planets[9].material.specular = new THREE.Color( 0x111111 );
+
+	var c = new THREE.Color( 0xED1A48 );
+	planets[9].material.color = c;
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
+
+
+
+	/***************************************************************************/
+	/**********************Planet 10 - Press Box Shuttle************************/
+	/***************************************************************************/
+
+	centerOrbit[10].rotation.set( 0, 0, degToRad(322) );
+
+	planets[10].position.set(0, 310, 0);
+	planets[10].scale.set(0.75, 0.75, 0.75);
+
+	/***************************************************************************/
+	/***************************************************************************/
+	/***************************************************************************/
 
 	//The following code lets us draw axes in the scene
 	//Code from http://rohitghatol.com/?p=388
@@ -215,7 +525,7 @@ function init() {
 	     return axes;
 	}
 
-	//scene.add(createAxes(500));
+	scene.add(createAxes(500));
 
 
 }
@@ -226,8 +536,19 @@ function animate() {
 	//pointLight.position.z = Math.sin(theta) * 800;
 
 	for (var i = 0; i < planets.length; i++) {
-		centerOrbit[i].rotation.z = theta;
+		//centerOrbit[i].rotation.z += planetTheta;
 	}
+
+	planets[0].rotation.set(degToRad(-35), theta, 0);
+	planets[1].rotation.set(0, 0, -theta * 2.5);
+	planets[2].rotation.set(0, -theta * 0.2, 0);
+	planets[3].rotation.set(theta * 0.5, 0, 0);
+	planets[4].rotation.set(-theta * 0.8, degToRad(-20), degToRad(-50));
+	planets[5].rotation.set( 0, theta * 0.4, 0 );
+
+	planets[7].rotation.set(0, 0, theta);
+	planets[8].rotation.set(theta * 0.5, 0, degToRad(-70));
+	planets[9].rotation.set(0, theta * 0.7, 0);
 
 	for (var i = 0; i < starGeometry.colors.length; i++) {
 		var brightness = Math.sin(theta * 4 + starOffsets[i]);
@@ -237,9 +558,10 @@ function animate() {
 		starGeometry.colors[i].b = newBrightness;
 	}
 
-	moonOrbit.rotation.z = theta*6.0;
+	moonOrbit.rotation.set(1, 0, theta*3.0);
 
 	theta += 0.005;
+
 
     requestAnimationFrame(animate);
  
@@ -260,6 +582,11 @@ function getRandomInt (min, max) {
 //Simple function to map values (similar to Processing's map() function)
 function map_range(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+//Simple function that converts degrees to radians. Returns negative for aesthetic simplicities sake. 
+function degToRad(deg) {
+	return -deg * (Math.PI / 180);
 }
 
 init();
